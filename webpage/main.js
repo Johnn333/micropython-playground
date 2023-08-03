a94f51922796e27bcec1f9de5c40fa4afc01ab00
Johnn333 <johnnygrice@hotmail.co.uk>
2023-08-03 15:32:11 +0100

    Now using pythonsed package, to replicate sed commands, compilation of C files seems to be going fine, still need to handle mpy-cross
    Pythonsed used in root.pack.br, to replicate UNIX sed. This was the final step of the micropython prerequisites before we can compile all the C files. This has now been done and the hex file fed back to the user, however for now it doesnt work, lots of things that could be doing this. In big need of a clean up so may do this before approaching mpy-cross

--- webpage/main.js
+++ webpage/main.js
@@ -1,3 +1,38 @@
 //JS file mainly to communicate with micropython.worker.js
 
 const micropythonWorker = new Worker('micropython.worker.js', {type:"module"});
+
+function buildMicropython(){ 
+    micropythonWorker.postMessage("Build");
+}
+
+micropythonWorker.onmessage = (e) => {
+    // console.log(e);
+    download(hex2ascii(toHexString(e.data)), "MICROBIT.hex");
+}
+document.getElementById("build").onclick = buildMicropython;
+
+// Awkward html to download a file that is stored locally
+function download(data, name) {
+    var a = document.createElement("a");
+    a.href = window.URL.createObjectURL(new Blob([data], {type: "text/plain"}));
+    a.download = name;
+    a.click();
+    a.remove();
+}
+
+// Convert a byte array to hex
+function toHexString(byteArray) {
+    return Array.prototype.map.call(byteArray, function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+}
+
+// Convert hex to ascii hex (ihex). Format read by the microbit.
+function hex2ascii(hexx) {
+    var hex = hexx.toString()
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
